d4a894c10c79862efdad5273192bb9bba5952c0d
Shashwat <shashwatakhileshshukla@gmail.com>
2025-12-19 16:01:05 +0530

    requirements issue render

--- deploy.sh
+++ deploy.sh
@@ -8,9 +8,9 @@ REGISTRY=$ACCOUNT_ID.dkr.ecr.$REGION.amazonaws.com
 
 aws ecr get-login-password --region $REGION | docker login --username AWS --password-stdin $REGISTRY
 
-# docker build -f backend.dockerfile -t cognitiveai-backend:latest .
-# docker tag cognitiveai-backend:latest $REGISTRY/cognitiveai-backend:latest
-# docker push $REGISTRY/cognitiveai-backend:latest
+docker build -f backend.dockerfile -t cognitiveai-backend:latest .
+docker tag cognitiveai-backend:latest $REGISTRY/cognitiveai-backend:latest
+docker push $REGISTRY/cognitiveai-backend:latest
 
 docker build -f frontend.dockerfile -t cognitiveai-frontend:latest .
 docker tag cognitiveai-frontend:latest $REGISTRY/cognitiveai-frontend:latest
